af7e364afb52c79dce1eed555785cff7e8cb1aa1
2307104544 <2307104544@qq.com>
2020-04-08 17:03:52 +0800

    V1.0

---
+++ src/main/java/com/xc/Y2XmApplication.java
@@ -0,0 +1,13 @@
+package com.xc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Y2XmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Y2XmApplication.class, args);
+    }
+
+}
